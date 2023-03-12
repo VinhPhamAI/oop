924e3c906d58327bebb1b95bc26074dd0c0e6772
VinhPhamAI <115690131+VinhPhamAI@users.noreply.github.com>
2023-03-12 20:28:18 +0700

    Add files via upload

---
+++ HW1/HelloWorld.java
@@ -0,0 +1,19 @@
+public class HelloWorld {
+
+    public static void main(String[] args) {
+
+        // Prints "Hello, World" in the terminal window.
+        System.out.println("Pham Quang Vinh");
+        System.out.println("22022648");
+        System.out.println("OOP");
+        System.out.println("VinhPhamAI");
+        System.out.println("22022648@vnu.edu.vn");
+        int n = 9;
+        while (n>0) {
+            System.out.println(n + " bottles of beer on the wall, " +n + " bottles of beer.");
+            System.out.println("Take one down, pass it around,");
+            n--;
+        }
+        System.out.print("No more bottles of beer on the wall.");
+    }
+}
