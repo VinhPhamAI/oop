daef4d03489a4844cb33364d9d7cd1d1a4601a97
Pham Quang Vinh <viymhg1234@gmail.com>
2023-03-12 21:57:17 +0700

    commit

--- HelloWorld.java
+++ HelloWorld.java
@@ -3,14 +3,10 @@ public class HelloWorld {
     public static void main(String[] args) {
 
         // Prints "Hello, World" in the terminal window.
-        System.out.println("Pham Quang Vinh");
-        System.out.println("22022648");
-        System.out.println("OOP");
-        System.out.println("VinhPhamAI");
-        System.out.println("22022648@vnu.edu.vn");
+        System.out.println("Pham Quang Vinh" + '\t' + "22022648" +'\t' + "OOP" + '\t' + "VinhPhamAI" + '\t' + "22022648@vnu.edu.vn");
         int n = 9;
         while (n>0) {
-            System.out.println(n + " bottles of beer on the wall, " +n + " bottles of beer.");
+            System.out.println(n + " bottles of beer on the wall, " + n + " bottles of beer.");
             System.out.println("Take one down, pass it around,");
             n--;
         }
