51e424f658a4d31fb824f7462de8171afcc6a879
VinhPhamAI <115690131+VinhPhamAI@users.noreply.github.com>
2023-03-12 23:26:45 +0700

    Add files via upload

---
+++ HW1/howMany.java
@@ -0,0 +1,9 @@
+public class howMany {
+
+    public static void main(String[] args) {
+        int n = args.length;
+        System.out.print("You entered " + n + " command-line argument");
+        if (n == 1) {System.out.println(".");}
+        else{System.out.println("s.");}
+    }
+}
