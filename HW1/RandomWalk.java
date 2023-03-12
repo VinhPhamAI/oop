e7efc558630e69fba8c4269efbbc3177099a4e9d
VinhPhamAI <115690131+VinhPhamAI@users.noreply.github.com>
2023-03-13 04:07:00 +0700

    Add files via upload

---
+++ HW1/RandomWalk.java
@@ -0,0 +1,134 @@
+import java.util.Scanner;
+import java.lang.Math;
+
+import static java.lang.Math.abs;
+
+public class RandomWalk {
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        StdDraw.setScale(-n - 0.5, n + 0.5);
+        StdDraw.clear(StdDraw.GRAY);
+        StdDraw.enableDoubleBuffering();
+
+        int x = 0, y = 0;
+        int steps = 0;
+        while (abs(x) < n && abs(y) < n) {
+            StdDraw.setPenColor(StdDraw.WHITE);
+            StdDraw.filledSquare(x, y, 0.45);
+            if(x==y && (x!=0 && y!=0) && (x>0 && y>0))
+            {
+                y++;
+                steps++;
+
+                StdDraw.filledSquare(x, y, 0.45);
+                StdDraw.show();
+                StdDraw.pause(40);
+                y++;
+                steps++;
+                StdDraw.setPenColor(StdDraw.BLUE);
+                StdDraw.filledSquare(x, y, 0.45);
+                StdDraw.show();
+                StdDraw.pause(40);
+                continue;
+            }
+            if(x==y && (x!=0 && y!=0) && (x<0 && y<0))
+            {
+                x++;
+                steps++;
+                StdDraw.filledSquare(x, y, 0.45);
+                StdDraw.show();
+                StdDraw.pause(40);
+                x++;
+                steps++;
+                StdDraw.setPenColor(StdDraw.BLUE);
+                StdDraw.filledSquare(x, y, 0.45);
+                StdDraw.show();
+                StdDraw.pause(40);
+                continue;
+            }
+            if(-x == y && (x!=0 && y!=0) && (x>0 && y<0)) {
+                y++;
+                steps++;
+                StdDraw.filledSquare(x, y, 0.45);
+                StdDraw.show();
+                StdDraw.pause(40);
+                y++;
+                steps++;
+                StdDraw.setPenColor(StdDraw.BLUE);
+                StdDraw.filledSquare(x, y, 0.45);
+                StdDraw.show();
+                StdDraw.pause(40);
+                continue;
+            }
+            if(-x == y && (x!=0 && y!=0) && (x<0 && y>0)) {
+                y--;
+                steps++;
+                StdDraw.filledSquare(x, y, 0.45);
+                StdDraw.show();
+                StdDraw.pause(40);
+                y--;
+                steps++;
+                StdDraw.setPenColor(StdDraw.BLUE);
+                StdDraw.filledSquare(x, y, 0.45);
+                StdDraw.show();
+                StdDraw.pause(40);
+                continue;
+            }
+            if(x==y && x==0)
+            {
+                x++;
+                steps++;
+                StdDraw.setPenColor(StdDraw.BLUE);
+                StdDraw.filledSquare(x, y, 0.45);
+                StdDraw.show();
+                StdDraw.pause(40);
+
+                continue;
+            }
+            if(x>y && abs(x) > abs(y))
+            {
+                y++;
+                steps++;
+                StdDraw.setPenColor(StdDraw.BLUE);
+                StdDraw.filledSquare(x, y, 0.45);
+                StdDraw.show();
+                StdDraw.pause(40);
+                continue;
+            }
+            if(x>y && abs(x) < abs(y))
+            {
+                x++;
+                steps++;
+                StdDraw.setPenColor(StdDraw.BLUE);
+                StdDraw.filledSquare(x, y, 0.45);
+                StdDraw.show();
+                StdDraw.pause(40);
+                continue;
+            }
+            if(x<y && abs(x) < abs(y))
+            {
+                x--;
+                steps++;
+                StdDraw.setPenColor(StdDraw.BLUE);
+                StdDraw.filledSquare(x, y, 0.45);
+                StdDraw.show();
+                StdDraw.pause(40);
+                continue;
+            }
+            if(x < y && abs(x) > y )
+            {
+                y--;
+                steps++;
+                StdDraw.setPenColor(StdDraw.BLUE);
+                StdDraw.filledSquare(x, y, 0.45);
+                StdDraw.show();
+                StdDraw.pause(40);
+                continue;
+            }
+        }
+        StdOut.println("Total steps = " + steps);
+    }
+
+}
