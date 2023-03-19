5c1fea7d7aae2f0471264eeb9291f159cb8dc83a
Pham Quang Vinh <115690131+VinhPhamAI@users.noreply.github.com>
2023-03-19 23:02:54 +0700

    Add files via upload

---
+++ HW2/src/main/java/Circle.java
@@ -0,0 +1,28 @@
+public class Circle {
+    private Point center;
+    private double radius;
+
+    public Circle (Point center, double radius)
+    {
+        this.center = center;
+        this.radius = radius;
+    }
+    public Point getCenter()
+    {
+        return center;
+    }
+    public double getRadius()
+    {
+        return radius;
+    }
+    public boolean intersects (Circle other)
+    {
+        double distance = center.distance(other.getCenter());
+        return distance < radius + other.getRadius();
+    }
+    public boolean contains (Point point)
+    {
+        double distance = center.distance(point);
+        return distance < radius;
+    }
+}
